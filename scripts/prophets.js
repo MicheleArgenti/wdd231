373a1e3f0f066d0bfbfc67893dc29c6e683f7c29
raghork <michele.argenti000@gmail.com>
2025-05-15 22:10:27 +0200

    Added some styling

---
+++ scripts/prophets.js
@@ -0,0 +1,34 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json'
+
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.prophets);
+    displayProphets(data.prophets);
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let otherInfromation = document.createElement('p');
+        otherInfromation.innerHTML = 'Date of birth: ' + prophet.birthdate + '<br>' + 'Place of Birth ' + prophet.birthplace
+        fullName.textContent = prophet.name;
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} Prophet`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(otherInfromation);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
+
+getProphetData();
